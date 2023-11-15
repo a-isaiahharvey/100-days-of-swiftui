e976afee2031af4770141656126925206ac23aea
Allister Isaiah Harvey <41553768+a-isaiahharvey@users.noreply.github.com>
2023-11-15 12:16:26 -0400

    Add tangled Swift file

---
+++ day-007/Day7.swift
@@ -0,0 +1,13 @@
+func isUppercase(_ string: String) -> Bool {
+    string == string.uppercased()
+}
+
+let string = "HELLO, WORLD"
+let result = isUppercase(string)
+
+func printTimesTables(for number: Int) {
+    for i in 1...12 {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+printTimesTables(for: 5)
